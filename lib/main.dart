b316778ce06c6a8213fbe782755dd4794e3ac44b
bonchangkoo <bonchang.koo@deliveryhero.co.kr>
2019-08-30 16:41:58 +0900

    스크롤 처리

--- lib/main.dart
+++ lib/main.dart
@@ -50,131 +50,133 @@ class MyCustomFormState extends State<MyCustomForm> {
     // Build a Form widget using the _formKey created above.
     return Form(
       key: _formKey,
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-            child: Row(children: <Widget>[
-              Container(
-                width: 100,
-                child: Text("방문 일자"),
-              ),
-              Container(
-                width: 200,
-                child: TextFormField(
-                  initialValue:
-                      DateFormat("yyyy-MM-dd").format(DateTime.now()),
-                  validator: (value) {
-                    if (value.isEmpty) {
-                      return '방문 일자를 입력해주세요';
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.numberWithOptions(
-                      decimal: true, signed: true),
-                ),
-              )
-            ]),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-            child: Row(children: <Widget>[
-              Container(
-                width: 100,
-                child: Text("방문자 이름"),
-              ),
-              Container(
-                  width: 200,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return '방문자 이름을 입력해주세요.';
-                      }
-                      return null;
-                    },
-                  ))
-            ]),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-            child: Row(children: <Widget>[
-              Container(
-                width: 100,
-                child: Text("방문자 소속"),
-              ),
-              Container(
-                  width: 200,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return '방문자 소속을 입력해주세요.';
-                      }
-                      return null;
-                    },
-                  ))
-            ]),
-          ),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
-            child: Row(children: <Widget>[
-              Container(
-                width: 100,
-                child: Text("방문 목적"),
-              ),
-              Container(
-                  width: 200,
-                  child: TextFormField(
-                    validator: (value) {
-                      if (value.isEmpty) {
-                        return '방문 목적을 입력해주세요.';
-                      }
-                      return null;
-                    },
-                  ))
-            ]),
-          ),
-          Padding(
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
               padding:
-                  const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+              const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
               child: Row(children: <Widget>[
                 Container(
                   width: 100,
-                  child: Text("접견자 이름"),
+                  child: Text("방문 일자"),
+                ),
+                Container(
+                  width: 200,
+                  child: TextFormField(
+                    initialValue:
+                    DateFormat("yyyy-MM-dd").format(DateTime.now()),
+                    validator: (value) {
+                      if (value.isEmpty) {
+                        return '방문 일자를 입력해주세요';
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.numberWithOptions(
+                        decimal: true, signed: true),
+                  ),
+                )
+              ]),
+            ),
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+              child: Row(children: <Widget>[
+                Container(
+                  width: 100,
+                  child: Text("방문자 이름"),
                 ),
                 Container(
                     width: 200,
                     child: TextFormField(
                       validator: (value) {
                         if (value.isEmpty) {
-                          return '접견자 이름을 입력해주세요.';
+                          return '방문자 이름을 입력해주세요.';
                         }
                         return null;
                       },
                     ))
-              ])),
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-            child: RaisedButton(
-              onPressed: () {
-                // Validate returns true if the form is valid, or false
-                // otherwise.
-                if (_formKey.currentState.validate()) {
-                  // If the form is valid, display a Snackbar.
-                  Scaffold.of(context)
-                      .showSnackBar(SnackBar(content: Text('제출하는 중입니다.')));
-                }
-              },
-              child: Text('제출하기'),
+              ]),
             ),
-          ),
-        ],
-      ),
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+              child: Row(children: <Widget>[
+                Container(
+                  width: 100,
+                  child: Text("방문자 소속"),
+                ),
+                Container(
+                    width: 200,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return '방문자 소속을 입력해주세요.';
+                        }
+                        return null;
+                      },
+                    ))
+              ]),
+            ),
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+              child: Row(children: <Widget>[
+                Container(
+                  width: 100,
+                  child: Text("방문 목적"),
+                ),
+                Container(
+                    width: 200,
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value.isEmpty) {
+                          return '방문 목적을 입력해주세요.';
+                        }
+                        return null;
+                      },
+                    ))
+              ]),
+            ),
+            Padding(
+                padding:
+                const EdgeInsets.symmetric(vertical: 4.0, horizontal: 16.0),
+                child: Row(children: <Widget>[
+                  Container(
+                    width: 100,
+                    child: Text("접견자 이름"),
+                  ),
+                  Container(
+                      width: 200,
+                      child: TextFormField(
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return '접견자 이름을 입력해주세요.';
+                          }
+                          return null;
+                        },
+                      ))
+                ])),
+            Padding(
+              padding:
+              const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+              child: RaisedButton(
+                onPressed: () {
+                  // Validate returns true if the form is valid, or false
+                  // otherwise.
+                  if (_formKey.currentState.validate()) {
+                    // If the form is valid, display a Snackbar.
+                    Scaffold.of(context)
+                        .showSnackBar(SnackBar(content: Text('제출하는 중입니다.')));
+                  }
+                },
+                child: Text('제출하기'),
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
