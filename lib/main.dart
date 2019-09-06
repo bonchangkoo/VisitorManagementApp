763e25fa3d206b65833af8d77a2248fb561d9b91
heesoo.kim <heesoo.kim@deliveryhero.co.kr>
2019-09-06 16:07:51 +0900

    render overflow 수정

--- lib/main.dart
+++ lib/main.dart
@@ -75,21 +75,20 @@ class MyCustomFormState extends State<MyCustomForm> {
                     width: 100,
                     child: Text("방문 일자"),
                   ),
-                  Container(
-                    width: 200,
-                    child: TextFormField(
-                      initialValue:
-                          DateFormat("yyyy-MM-dd").format(DateTime.now()),
-                      validator: (value) {
-                        if (value.isEmpty) {
-                          return '방문 일자를 입력해주세요';
-                        }
-                        return null;
-                      },
-                      keyboardType: TextInputType.numberWithOptions(
-                          decimal: true, signed: true),
-                    ),
-                  )
+                  Expanded(
+                      child: TextFormField(
+                        initialValue:
+                        DateFormat("yyyy-MM-dd").format(DateTime.now()),
+                        validator: (value) {
+                          if (value.isEmpty) {
+                            return '방문 일자를 입력해주세요';
+                          }
+                          return null;
+                        },
+                        keyboardType: TextInputType.numberWithOptions(
+                            decimal: true, signed: true),
+                      ),
+                  ),
                 ]),
               ),
               Padding(
@@ -100,8 +99,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                     width: 100,
                     child: Text("방문자 이름"),
                   ),
-                  Container(
-                      width: 200,
+                  Expanded(
                       child: TextFormField(
                         validator: (value) {
                           if (value.isEmpty) {
@@ -149,8 +147,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                     width: 100,
                     child: Text("방문 목적"),
                   ),
-                  Container(
-                      width: 200,
+                  Expanded(
                       child: TextFormField(
                         validator: (value) {
                           if (value.isEmpty) {
@@ -169,8 +166,7 @@ class MyCustomFormState extends State<MyCustomForm> {
                       width: 100,
                       child: Text("접견자 이름"),
                     ),
-                    Container(
-                        width: 200,
+                    Expanded(
                         child: TextFormField(
                           validator: (value) {
                             if (value.isEmpty) {
