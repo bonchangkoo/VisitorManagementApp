1be9da6342de8c9b42b07676133628afa5d89964
Chanho.Seong <chanho.seong@deliveryhero.co.kr>
2019-10-04 15:39:47 +0900

    디펜던시 추가 및 AuthClient 적용 코드 추가

--- lib/bloc/google_sheet_controller.dart
+++ lib/bloc/google_sheet_controller.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'package:googleapis/sheets/v4.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:http/http.dart' as http;
+import 'package:url_launcher/url_launcher.dart';
 
 class GoogleSheetController {
 
@@ -13,33 +14,31 @@ class GoogleSheetController {
   static GoogleSheetController INSTANCE = GoogleSheetController._innerConstructor();
 
   final _streamController = StreamController<String>();
-  final _httpClient = http.Client();
-  Stream<String> get urlStream => _streamController.stream;
 
   final _SCOPES = const [SheetsApi.SpreadsheetsScope];
   final _clientId = ClientId('863738001853-pvigji6dbovcg42ii1o22msffp8acbvf.apps.googleusercontent.com', 'P6-7q1ywRn6E43g9a6VsNfb1');
 
-  void requestCertificate() {
-    obtainAccessCredentialsViaUserConsent(_clientId, _SCOPES, _httpClient, onReceivedPrompt)
-        .catchError((Exception e) {
-          _streamController.sink.addError(e);
-        }).then((accessCredentials) {
-          onReceiveAuthenticatedClient(authenticatedClient(_httpClient, accessCredentials));
-        });
+  Future<http.Client> _httpClient() async {
+    final AuthClient authClient = await clientViaUserConsent(_clientId, _SCOPES, (url) {
+      launch(url);
+    });
+    return authClient;
   }
+  Stream<String> get urlStream => _streamController.stream;
 
-  void onReceiveAuthenticatedClient(AuthClient authClient) {
-    SheetsApi(authClient, );
-  }
 
-  void onReceivedPrompt(String url) {
-    print("@## =>> $url");
-    _streamController.sink.add(url);
+
+  void requestSheetInfo() async {
+    http.Client client = await _httpClient();
+    Spreadsheet sheet = await SheetsApi(client).spreadsheets.get('1ztkYwM6Kz0F8X-PcksSqStdbMqpvh1B55Wo81nmd5t4');
+    for (Sheet s in sheet.sheets) {
+      print("==> $s.");
+    }
+    client.close();
   }
 
   void close() {
     _streamController.close();
-    _httpClient.close();
   }
 
 }
