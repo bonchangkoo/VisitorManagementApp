ad287edb3de988a680dc9aaf2bf8a33056cd5ac9
박홍철(Park, Hongchul) <hongchul.park@deliveryhero.co.kr>
2019-12-06 16:15:05 +0900

    키 추가

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/a201907081/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/a201907081/flutter/VisitorManagementApp"
+export "FLUTTER_TARGET=/Users/a201907081/flutter/VisitorManagementApp/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/a201907081/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
